894422bc83bbdfefd607fb1da25a6de44ba61661
SnuggleTrouble <stabjo@outlook.com>
2022-01-18 14:28:07 +0100

    first commit. itereation 1 and 2 complete

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 
-const hbs = require('hbs');
-const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const hbs = require("hbs");
+const path = require("path");
+const PunkAPIWrapper = require("punkapi-javascript-wrapper");
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Register the location for handlebars partials here:
 
@@ -18,8 +18,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get("/beers", (req, res) => {
+  res.render("");
+});
+
+app.get("/random-beer", (req, res) => {
+  res.render("")
+})
+
+app.listen(3000, () => console.log("🏃‍ on port 3000"));
